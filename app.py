d87c4dc57b6bce2e6ff2143a28a453659592713f
Matheus Henrique da Silva <64704184+ErDragons@users.noreply.github.com>
2022-05-22 16:15:41 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -7,16 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Teste vai"
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")
+    return "Teste vai - Full03"
 
 if __name__ == '__main__':
     app.run()
